a18b27fc3622693ef7d2d2779127fc2bc47b85ab
Yeyzon <yeyzon75@gmail.com>
2023-03-25 13:42:37 -0600

    Se modifico home.css

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/products" element={<Products />} />
         </Routes>
       </Router>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,47 @@
+import React from "react";
 function Home() {
   return (
-    <>
-      <div className="Home__content">
-        {/* Aquí va el contenido de la página Home */}
-        <h1>Holaaaaaa</h1>
-        <h1>Holaaaaaa</h1>
-        <h1>Holaaaaaa</h1>
-        <h1>Holaaaaaa</h1>
+    <div className="home">
+      <div className="home__banner">
+        <h1>Bienvenido a ShopEase</h1>
+        <p>
+          Explora nuestra selección de productos destacados y ofertas especiales
+        </p>
+        <button>Explorar productos</button>
       </div>
-    </>
+      <div className="home__services">
+        <div className="home__services-item">
+          <i className="fas fa-shipping-fast"></i>
+          <h2>Envío gratuito</h2>
+          <p>En compras superiores a $50</p>
+        </div>
+        <div className="home__services-item">
+          <i className="fas fa-undo"></i>
+          <h2>Devoluciones fáciles</h2>
+          <p>Hasta 30 días después de la compra</p>
+        </div>
+        <div className="home__services-item">
+          <i className="fas fa-wallet"></i>
+          <h2>Pagos seguros</h2>
+          <p>Utilizamos encriptación SSL</p>
+        </div>
+      </div>
+      <div className="home__categories">
+        <h2>Explora nuestras categorías de productos</h2>
+        <div className="home__category-item">
+          <img src="https://via.placeholder.com/150" alt="Categoría 1" />
+          <h3>Categoría 1</h3>
+        </div>
+        <div className="home__category-item">
+          <img src="https://via.placeholder.com/150" alt="Categoría 2" />
+          <h3>Categoría 2</h3>
+        </div>
+        <div className="home__category-item">
+          <img src="https://via.placeholder.com/150" alt="Categoría 3" />
+          <h3>Categoría 3</h3>
+        </div>
+      </div>
+    </div>
   );
 }
 
