c2dae72fcb0305971fc3344d32cbb3f1319395c9
Yeyzon <yeyzon75@gmail.com>
2023-03-30 11:49:34 -0600

    Se hizo completamente responsivo Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+
 const rutaBaseImage = process.env.PUBLIC_URL + "/images/";
 
 function Home() {
   return (
-    <div className="home">
-      <div className="home__banner">
+    <main className="home">
+      <section className="home__banner">
         <div className="home__banner-overlay">
           <h1>Bienvenido a ShopEase</h1>
           <p>
@@ -17,8 +18,8 @@ function Home() {
             <button>Explorar productos</button>
           </Link>
         </div>
-      </div>
-      <div className="home__services">
+      </section>
+      <section className="home__services">
         <div className="home__services-item">
           <i className="fas fa-shipping-fast"></i>
           <h2>Envío gratuito</h2>
@@ -29,23 +30,27 @@ function Home() {
           <h2>Pagos seguros</h2>
           <p>Utilizamos encriptación SSL</p>
         </div>
-      </div>
-      <h2>Explora nuestras categorías de productos</h2>
-      <div className="home__categories">
-        <div className="home__category-item">
-          <img src={rutaBaseImage + "calzado1.jpg"} alt="Categoría 1" />
-          <h3>Calzado</h3>
+      </section>
+      <section className="home__categories">
+        <div className="home__categories-header">
+          <h2>Explora nuestras categorías de productos</h2>
         </div>
-        <div className="home__category-item">
-          <img src={rutaBaseImage + "ropa1.jpg"} alt="Categoría 2" />
-          <h3>Ropa</h3>
+        <div className="home__categories-items">
+          <div className="home__category-item">
+            <img src={rutaBaseImage + "calzado1.jpg"} alt="Categoría 1" />
+            <h3>Calzado</h3>
+          </div>
+          <div className="home__category-item">
+            <img src={rutaBaseImage + "ropa1.jpg"} alt="Categoría 2" />
+            <h3>Ropa</h3>
+          </div>
+          <div className="home__category-item">
+            <img src={rutaBaseImage + "varios1.jpg"} alt="Categoría 3" />
+            <h3>Varios</h3>
+          </div>
         </div>
-        <div className="home__category-item">
-          <img src={rutaBaseImage + "varios1.jpg"} alt="Categoría 3" />
-          <h3>Varios</h3>
-        </div>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 }
 
