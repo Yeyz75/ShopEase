ec00fe88410f5ff2a6b6e1800a91f3b4574744e0
Yeyzon <yeyzon75@gmail.com>
2023-03-31 08:37:30 -0600

    Iremos paso a paso con la api

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,42 +1,35 @@
-import React, { useState } from "react";
-import Product from "../../components/Product/product";
 import "./Products.css";
+import Product from "../../components/Product/product";
 
 const Products = () => {
-  const products = [
-    {
-      id: 1,
-      name: "Product 1",
-      image: "https://via.placeholder.com/150",
-      price: "$10",
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      image: "https://via.placeholder.com/150",
-      price: "$20",
-    },
-    {
-      id: 3,
-      name: "Product 3",
-      image: "https://via.placeholder.com/150",
-      price: "$30",
-    },
-  ];
-
   return (
-    <div className="products-container">
-      <div className="products">
-        {products.map((product) => (
+    <main id="main-content">
+      <header>
+        <h1>Descubre nuestra selección de productos</h1>
+        <p>
+          En ShopEase, ofrecemos una amplia variedad de productos de alta
+          calidad.
+        </p>
+      </header>
+      <section className="products-container">
+        <h2>Our Products</h2>
+        <div className="products">
           <Product
-            key={product.id}
-            name={product.name}
-            image={product.image}
-            price={product.price}
+            name="Product 1"
+            description="This is the description of product 1."
+            price="19.99"
+            image="https://via.placeholder.com/150"
           />
-        ))}
-      </div>
-    </div>
+          <Product
+            name="Product 2"
+            description="This is the description of product 2."
+            price="29.99"
+            image="https://via.placeholder.com/150"
+          />
+          {/* Agrega más productos aquí */}
+        </div>
+      </section>
+    </main>
   );
 };
 
