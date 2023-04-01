f021fff68fe5b0016f5eae3a29e283dc9e3a69f5
Yeyzon <yeyzon75@gmail.com>
2023-04-01 11:49:51 -0600

    Se arreglo los colores de mi Navbar Y Footer

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import Product from "../../components/Product";
 import getProductBrands from "../../services/productapi";
+import "./Products.css";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
-  const [numProductsToShow] = useState(10);
+  const [numProductsToShow] = useState(20);
   const [loaded, setLoaded] = useState(false);
 
   const handleClick = async () => {
@@ -17,16 +18,21 @@ const Products = () => {
 
   return (
     <div className="products">
-      <h1>Bienvenidos a ShopEase</h1>
-      <p>
-        Aquí se muestran todos los productos disponibles en nuestra tienda
-        online:
-      </p>
-      {loaded &&
-        products
-          .slice(0, numProductsToShow)
-          .map((product) => <Product key={product.id} product={product} />)}
-
+      <div className="header">
+        <h1>Bienvenidos a ShopEase</h1>
+        <p>
+          Aquí se muestran todos los productos disponibles en nuestra tienda
+          online:
+        </p>
+      </div>
+      <div className="product-wrapper">
+        {loaded &&
+          products.slice(0, numProductsToShow).map((product) => (
+            <div key={product.id} className="product-container">
+              <Product product={product} />
+            </div>
+          ))}
+      </div>
       <button onClick={handleClick}>Mostrar más</button>
     </div>
   );
