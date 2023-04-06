a8bca7e18c0c029401fbe99c1f2db8f475e031a6
Yeyzon <yeyzon75@gmail.com>
2023-04-06 14:20:25 -0600

    Se ha añadido y mejorado Firestore

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,32 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { db } from "../../services/firebase";
-
 import "./AdminPage.css";
+import { db, collection, getDocs } from "../../services/firebase";
 
 const AdminPage = () => {
-  const [products, setProducts] = useState([]);
-
-  // Use useEffect hook to get products from Firebase on component mount
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const data = await db.collection("products").get(); // Obtiene los datos de la colección "products" de Firebase
-      setProducts(data.docs.map((doc) => ({ id: doc.id, ...doc.data() }))); // Mapea los datos obtenidos y los actualiza en el estado local del componente
-    };
-    fetchProducts(); // Ejecuta la función fetchProducts en el montaje del componente
-  }, []);
-
-  // Agregar lógica para manejar productos
-  const handleAddProduct = () => {
-    // Agregar un nuevo producto a Firebase y actualizar el estado local del componente
-  };
-
-  const handleUpdateProduct = (productId, updatedProductData) => {
-    // Actualizar un producto existente en Firebase y actualizar el estado local del componente
-  };
-
-  const handleDeleteProduct = (productId) => {
-    // Eliminar un producto de Firebase y actualizar el estado local del componente
-  };
+  const productsRef = collection(db, "products");
+  getDocs(productsRef)
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+      });
+    })
+    .catch((error) => {
+      console.log("Error getting documents: ", error);
+    });
 
   return (
     <div>
