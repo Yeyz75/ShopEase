48cdf18efb29eddc85eb5ca1eb54fd768c252242
Yeyzon <yeyzon75@gmail.com>
2023-04-11 10:24:51 -0600

    Products ya funciona, trae los productos registrados

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,20 +1,46 @@
-import React, { useState } from "react";
-import Product from "../../components/Product";
-import getProductBrands from "../../services/productapi";
+import { useState, useEffect } from "react";
 import "./Products.css";
+import {
+  db,
+  collection,
+  getDocs,
+  ref,
+  storage,
+  getDownloadURL,
+} from "../../services/firebase";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
-  const [numProductsToShow] = useState(20);
-  const [loaded, setLoaded] = useState(false);
 
-  const handleClick = async () => {
-    if (!loaded) {
-      const data = await getProductBrands();
-      setProducts(data);
-      setLoaded(true);
-    }
-  };
+  useEffect(() => {
+    const fetchProducts = async () => {
+      // Obtener la colección de productos de Firestore
+      const productsCollection = collection(db, "products");
+
+      // Obtener los documentos de la colección
+      const snapshot = await getDocs(productsCollection);
+
+      // Convertir los documentos a un arreglo de objetos
+      const productsArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      // Obtener la URL de descarga de la imagen para cada producto
+      const productsWithUrls = await Promise.all(
+        productsArray.map(async (product) => {
+          const imageRef = ref(storage, product.image);
+          const imageUrl = await getDownloadURL(imageRef);
+          return { ...product, imageUrl };
+        })
+      );
+
+      // Actualizar el estado con los productos y sus URLs de descarga de imagen
+      setProducts(productsWithUrls);
+    };
+
+    fetchProducts();
+  }, []);
 
   return (
     <div className="products">
@@ -26,14 +52,15 @@ const Products = () => {
         </p>
       </div>
       <div className="product-wrapper">
-        {loaded &&
-          products.slice(0, numProductsToShow).map((product) => (
-            <div key={product.id} className="product-container">
-              <Product product={product} />
-            </div>
-          ))}
+        {products.map((product) => (
+          <div key={product.id} className="product">
+            <h2>{product.productName}</h2>
+            <img src={product.imageUrl} alt={product.nombre} />
+            <p>{product.description}</p>
+            <p>Precio: ${product.price}</p>
+          </div>
+        ))}
       </div>
-      <button onClick={handleClick}>Mostrar más</button>
     </div>
   );
 };
